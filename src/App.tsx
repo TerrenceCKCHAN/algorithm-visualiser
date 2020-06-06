4cff28c726579b4ac64fd036367d13a51918e984
TerrenceCKCHAN <ckterrencechan@gmail.com>
2020-06-06 21:06:51 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      This is an algorithm visualiser for education purposes
+    </div>
+  );
+}
+
+export default App;
