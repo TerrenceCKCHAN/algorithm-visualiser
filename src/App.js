fbc13859347da9f37a4a49546a950b2031f2f203
TerrenceCKCHAN <ckterrencechan@gmail.com>
2020-09-16 23:06:39 +0100

    Initialize Sorting Visualizer, display constantly updated array values

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import SortingVisualizer from './components/SortingVisualizer/SortingVisualizer';
 
 function App() {
   return (
     <div>
       <div className="App">
-        This is an algorithm visualiser for education purposes
+        <SortingVisualizer/>
       </div>
     </div>
   );
---
+++ src/components/SortingVisualizer/SortingVisualizer.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+
+function SortingVisualizer() {
+    const[array, setArray] = useState([]);
+
+    useEffect(() => setArray(resetArray()))
+
+    return (
+        <>
+            <h1>
+                Visualizer
+            </h1>
+            { 
+                array.map((value, idx) => {
+                    return (
+                    <div className="array-columns" key={idx}>
+                        {value}
+                    </div>
+                    )
+                })
+            }  
+        </>
+    );
+}
+
+function resetArray() {
+    const array = Array.from({length: 40}, () => Math.floor(Math.random() * 1000));
+    console.log(array);
+    return array;
+}
+
+export default SortingVisualizer;
