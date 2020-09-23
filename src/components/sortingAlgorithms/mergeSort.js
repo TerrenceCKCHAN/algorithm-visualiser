ba5b48dcf2a7501402e2c842386333140f954fb5
TerrenceCKCHAN <ckterrencechan@gmail.com>
2020-09-23 20:00:05 +0100

    Create getAnimations function

---
+++ src/components/sortingAlgorithms/mergeSort.js
@@ -0,0 +1,9 @@
+
+export function getAnimations(array) {
+  // returns a list of animations from merge sort that we will use to animate our algorithm
+  const animations = [];
+  auxArray = array.slice();
+  // TODO Implement helper to sort array using merge sort
+  helper()
+  return animations;
+}
