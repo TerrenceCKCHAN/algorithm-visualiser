5636511bd6ffa69ae3001091dba83367a2a60713
TerrenceCKCHAN <ckterrencechan@gmail.com>
2020-09-25 18:02:49 +0100

    Implement merge function

--- src/components/sortingAlgorithms/mergeSort.js
+++ src/components/sortingAlgorithms/mergeSort.js
@@ -1,9 +1,50 @@
+function helper(auxArray) {
+  // TODO Implement merge sort here
+  console.log(auxArray);
+  return -1;
+}
 
-export function getAnimations(array) {
+function merge(array, start, middle, end, auxArray, animations) {
+  let i = start;
+  let j = middle + 1;
+  let x = start;
+
+  while (i <= middle && j <= end) {
+    animations.push([i, j]);
+    animations.push([i, j]);
+    if (auxArray[i] <= auxArray[j]) {
+      animations.push([x, auxArray[i]]);
+      array[x] = auxArray[i];
+      i += 1;
+      x += 1;
+    } else {
+      animations.push([x, auxArray[j]]);
+      array[x] = auxArray[j];
+      j += 1;
+      x += 1;
+    }
+  }
+
+  while (i <= middle) {
+    animations.push([i, i]);
+    array[x] = auxArray[i];
+    i += 1;
+    x += 1;
+  }
+
+  while (j <= end) {
+    animations.push([j, j]);
+    array[x] = auxArray[j];
+    j += 1;
+    x += 1;
+  }
+}
+
+export default function getAnimations(array) {
   // returns a list of animations from merge sort that we will use to animate our algorithm
   const animations = [];
-  auxArray = array.slice();
+  const auxArray = array.slice();
   // TODO Implement helper to sort array using merge sort
-  helper()
+  helper(auxArray);
   return animations;
 }
